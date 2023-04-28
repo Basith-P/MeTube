523a2ac487e2d67946fb4c6972b704aa78e81ea6
Basith P <basithpofficial@gmail.com>
2023-04-28 10:34:15 +0530

    deploy: gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,23 @@ import {
 } from "./components";
 import RootLayout from "./layouts/RootLayout";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    errorElement: <NotFound />,
-    children: [
-      { path: "/", element: <Feed /> },
-      { path: "/video/:id", element: <VideoDetails /> },
-      { path: "/channel/:id", element: <ChannelDetails /> },
-      { path: "/search/:query", element: <SearchResults /> },
-      { path: "/:path(.*)", element: <NotFound /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootLayout />,
+      errorElement: <NotFound />,
+      children: [
+        { path: "/", element: <Feed /> },
+        { path: "/video/:id", element: <VideoDetails /> },
+        { path: "/channel/:id", element: <ChannelDetails /> },
+        { path: "/search/:query", element: <SearchResults /> },
+        { path: "/:path(.*)", element: <NotFound /> },
+      ],
+    },
+  ],
+  { basename: "/MeTube" }
+);
 
 const App = () => (
   <Box sx={{ bgcolor: "#000" }}>
